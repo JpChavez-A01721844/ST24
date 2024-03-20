15030be5e593ccfe629fba2c73f45214d3734d7d
Bryan Cortés <a01284228@itesm.mx>
2024-03-20 10:05:32 -0600

    Update (Square and rectangle)
    Actualizar paint.py en rama de Bryan.
    Contiene funciones de rectangulo y triangulo.

--- paint.py
+++ paint.py
@@ -10,12 +10,23 @@ Exercises
 """
 
 from turtle import *
-
 from freegames import vector
 
-
+def info_alumnos():
+  writer.up()
+  writer.goto(0,190)
+  writer.color('blue')
+  writer.write('Name Apellido Paterno Materno Matricula',align='left', font=('Arial', 10, 'normal'))
+  writer.goto(0,170)
+  writer.color('pink')
+  writer.write('NameApellidoPaternoMaternoMatricula',align='left',font=('Arial',10,'normal'))
+                                                                                
 def line(start, end):
-    """Draw line from start to end."""
+    """Draw line from start to end.
+    start - vector (x,y)
+    end - vector (x,y)
+    uo() - levanta pluma
+    down() - baja pluma"""
     up()
     goto(start.x, start.y)
     down()
@@ -43,12 +54,33 @@ def circle(start, end):
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(2):
+        forward(end.x - start.x)
+        left(90)
+        forward(end.x - (start.x)*2 )
+        left(90)
+
+
+    end_fill()
 
 
 def triangle(start, end):
     """Draw triangle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(3):
+        forward(end.x - start.x)
+        left(120)
+
+    end_fill()
 
 
 def tap(x, y):
@@ -65,19 +97,22 @@ def tap(x, y):
 
 
 def store(key, value):
-    """Store value in state at key."""
+    """Store value in state at key. Solo se llama cujando se va a modificar el shape"""
     state[key] = value
 
-
+# estado inicial del paint - diccionario - start - si el usuario ya dio un click mouse- shape - linea
 state = {'start': None, 'shape': line}
+# setup(ancho,alto, x_esq, suo, izq_wind, y_Esq.sup, izq_wind)
 setup(420, 420, 370, 0)
+# registra la funcion que atendera los eventos del mouse
 onscreenclick(tap)
 listen()
-onkey(undo, 'u')
+onkey(undo, 'u') # funcion/tecla
 onkey(lambda: color('black'), 'K')
 onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
+onkey(lambda: color('#33FF4F'), 'Z')
 onkey(lambda: color('red'), 'R')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
