16c6f8683bcfff31a44d4ff710b080e9de15e542
JpChavez-A01721844 <93736701+JpChavez-A01721844@users.noreply.github.com>
2024-03-20 09:48:08 -0600

    Update paint.py
    Rectangle y Triangle

--- paint.py
+++ paint.py
@@ -57,13 +57,34 @@ def circle(start, end):
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for _ in range(2):
+        forward(end.x - start.x)
+        left(90)
+        forward(end.y - start.y)
+        left(90)
+
+    end_fill()
 
 
 def triangle(start, end):
     """Draw triangle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
 
+    for _ in range(3):
+        goto(end.x, end.y)
+        goto(start.x, start.y)
+        right(120)
+
+    end_fill()
+  
 
 def tap(x, y):
     """Store starting point or draw shape."""
